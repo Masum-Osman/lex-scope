049b9500a1ba195b0747038fa487706ab3f7a710
Masum-Osman <ogkmasum@gmail.com>
2025-05-26 12:16:48 +0600

    feat(repository): implement MongoDB CRUD operations using best practices

--- modules/text/repository/repo.go
+++ modules/text/repository/repo.go
@@ -13,6 +13,7 @@ type TextRepository interface {
 	GetByID(ctx context.Context, id string) (*domain.Text, error)
 	Update(ctx context.Context, text *domain.Text) error
 	Delete(ctx context.Context, id string) error
+	List(ctx context.Context) ([]domain.Text, error)
 }
 
 type mongoTextRepository struct {
@@ -53,4 +54,21 @@ func (r *mongoTextRepository) Update(ctx context.Context, text *domain.Text) err
 func (r *mongoTextRepository) Delete(ctx context.Context, id string) error {
 	_, err := r.collection.DeleteOne(ctx, bson.M{"_id": id})
 	return err
+}
+
+func (r *mongoTextRepository) List(ctx context.Context) ([]domain.Text, error) {
+	cursor, err := r.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var texts []domain.Text
+	for cursor.Next(ctx) {
+		var t domain.Text
+		if err := cursor.Decode(&t); err == nil {
+			texts = append(texts, t)
+		}
+	}
+	return texts, nil
 }
