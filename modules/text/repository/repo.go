ce970784643df241d4ceece17cbef9278de883b2
Masum-Osman <ogkmasum@gmail.com>
2025-05-26 09:04:40 +0600

    feat(repository): add MongoDB implementation for TextRepository

---
+++ modules/text/repository/repo.go
@@ -0,0 +1,56 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/Masum-Osman/lex-scope/modules/text/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type TextRepository interface {
+	Save(ctx context.Context, text *domain.Text) error
+	GetByID(ctx context.Context, id string) (*domain.Text, error)
+	Update(ctx context.Context, text *domain.Text) error
+	Delete(ctx context.Context, id string) error
+}
+
+type mongoTextRepository struct {
+	collection *mongo.Collection
+}
+
+func NewTextRepository(db *mongo.Database) TextRepository {
+	return &mongoTextRepository{
+		collection: db.Collection("texts"),
+	}
+}
+
+func (r *mongoTextRepository) Save(ctx context.Context, text *domain.Text) error {
+	text.ID = ""
+	_, err := r.collection.InsertOne(ctx, text)
+	return err
+}
+
+func (r *mongoTextRepository) GetByID(ctx context.Context, id string) (*domain.Text, error) {
+	var text domain.Text
+	err := r.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&text)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &text, nil
+}
+
+func (r *mongoTextRepository) Update(ctx context.Context, text *domain.Text) error {
+	filter := bson.M{"_id": text.ID}
+	update := bson.M{"$set": bson.M{"content": text.Content, "user_id": text.UserID}}
+	_, err := r.collection.UpdateOne(ctx, filter, update)
+	return err
+}
+
+func (r *mongoTextRepository) Delete(ctx context.Context, id string) error {
+	_, err := r.collection.DeleteOne(ctx, bson.M{"_id": id})
+	return err
+}
